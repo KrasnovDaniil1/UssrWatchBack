b28ec61328d9ebe6c68d03ea9b6a8cedb70a2a14
KrasnovDaniil1 <kd69931@gmail.com>
2025-04-09 16:57:37 +0200

    add

--- app/database/session.py
+++ app/database/session.py
@@ -1,2 +1,8 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import URL, create_engine,text
+from config import settings
+
+
 def createDatabase():
     print("Создал таблицу")
