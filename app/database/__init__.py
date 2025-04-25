a5197598304dd863e839177131d4bd20939b4177
KrasnovDaniil1 <kd69931@gmail.com>
2025-04-25 21:56:07 +0200

    add

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,5 +1,20 @@
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from database.config import settings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+    
+    @property
+    def DATABASE_URL_asyncpg(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+settings = Settings()
 
 engine = create_async_engine(
     url = settings.DATABASE_URL_asyncpg,
--- app/database/config.py
+++ app/database/config.py
@@ -1,4 +1,3 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 
 from sqlalchemy import ForeignKey, text
 from sqlalchemy.orm import DeclarativeBase, class_mapper, mapped_column, Mapped, mapped_column
@@ -10,22 +9,6 @@ from typing import Type, Annotated
 import datetime
 
 # добавить alembic
-# добавить таблицы для черновиков
-
-class Settings(BaseSettings):
-    DB_HOST: str
-    DB_PORT: int
-    DB_USER: str
-    DB_PASS: str
-    DB_NAME: str
-
-    model_config = SettingsConfigDict(env_file=".env")
-    
-    @property
-    def DATABASE_URL_asyncpg(self):
-        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
-    
-settings = Settings()
 
 class Base(AsyncAttrs, DeclarativeBase):
     __abstract__ = True 
